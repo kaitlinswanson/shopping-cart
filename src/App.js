d1d0ba8f0a7e235032778d3f2978464d94e1657b
Kaitlin Swanson <kateswanson94@gmail.com>
2021-08-10 22:56:55 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Nav from './Nav';
+import Shop from './Shop';
+import Cart from './Cart';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Nav />
+        <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/shop" component={Shop}/>
+        <Route exact path="/cart" component={Cart}/>
+        </Switch>
     </div>
+    </Router>
   );
+}
+
+const Home = () => {
+  return (
+    <div>
+      <h1>HOME</h1>
+    </div>
+  )
 }
 
 export default App;
---
+++ src/Shop.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function Shop() {
+  return (
+    <div>
+        <h1>This is the shop page</h1>
+    </div>
+  );
+}
+
+export default Shop;
